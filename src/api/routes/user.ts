222bb2ea7d2f5ed149b3982b2f5522c7c2443aa3
Hieu Cao <hieu.cao@supermetrics.com>
2024-12-12 10:18:20 +0200

    Implement rough service for fetching users

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,11 +1,39 @@
 import { Router } from "express";
+import { getUserById, getUsers } from "../../services/userService";
 
 const route = Router();
 
-export default (app: Router) => {
+export default (app: Router): void => {
   app.use("/users", route);
 
-  route.get("/", (req, res) => {
-    res.send("Getting users");
+  route.get("/", async (req, res) => {
+    try {
+      const users = await getUsers();
+      res.json(users);
+    } catch (error) {
+      console.error("Error in / route: ", error);
+      res.status(500).json({ error: "Failed to fetch users" });
+    }
+  });
+
+  route.get("/:id", async (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+
+    if (isNaN(userId)) {
+      res.status(400).json({ error: "Invalid user ID" });
+      return;
+    }
+
+    try {
+      const user = await getUserById(userId);
+      if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+      }
+      res.json(user);
+    } catch (error) {
+      console.error("Error in /:id route: ", error);
+      res.status(500).json({ error: "Failed to fetch user" });
+    }
   });
 };
---
+++ src/services/userService.ts
@@ -0,0 +1,21 @@
+import { pool } from "../db";
+
+export const getUsers = async () => {
+  try {
+    const result = await pool.query("SELECT * from users");
+    return result.rows;
+  } catch (err) {
+    console.error("Error fetching users", err);
+    throw err;
+  }
+};
+
+export const getUserById = async (id: number) => {
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    return result.rows[0];
+  } catch (err) {
+    console.error("Error fetching user by ID", err);
+    throw err;
+  }
+};
